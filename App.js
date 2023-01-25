2478ab1341564d603cf29d3fe1f5b3494591bebb
kidscoots101 <calebhan08js@gmail.com>
2023-01-25 20:38:14 +0800

    Created a new Expo app

---
+++ App.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const s = () => {
+  return (
+    <View>
+      <Text>s</Text> s
+    </View>
+  );
+};
+
+export default s;
